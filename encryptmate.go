e39d445892610a0414b99730cbab6e6bf724f335
juan <jsburckhardt@gmail.com>
2018-04-30 23:27:55 +1000

    encrypting and decrypting with strings with env variable

---
+++ encryptmate.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+	encryptionKey := os.Getenv("GO_ENCRYPTION_KEY")
+	ciphertext := encrypt([]byte("Hello World"), encryptionKey)
+	fmt.Printf("Encrypted: %x\n", ciphertext)
+	plaintext := decrypt(ciphertext, encryptionKey)
+	fmt.Printf("Decrypted: %s\n", plaintext)
+	//encryptFile("sample.txt", []byte("Hello World"), "password1")
+	//fmt.Println(string(decryptFile("sample.txt", "password1")))
+}
