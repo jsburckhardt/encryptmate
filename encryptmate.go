2060bce8ee7c73084b8e38751e58cca2bbf423d7
juan <jsburckhardt@gmail.com>
2018-05-01 22:16:26 +1000

    included flags

--- encryptmate.go
+++ encryptmate.go
@@ -6,9 +6,9 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"io"
-	"os"
 )
 
 func createHash(key string) string {
@@ -21,10 +21,12 @@ func encrypt(data []byte, passphrase string) []byte {
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
+		fmt.Println("first")
 		panic(err.Error())
 	}
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println("second")
 		panic(err.Error())
 	}
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
@@ -35,28 +37,47 @@ func decrypt(data []byte, passphrase string) []byte {
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
+		fmt.Println("third")
 		panic(err.Error())
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
+		fmt.Println("fourth")
 		panic(err.Error())
 	}
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
+		fmt.Println("fifth")
 		panic(err.Error())
 	}
 	return plaintext
 }
 
 func main() {
-	fmt.Println("Starting the application...")
-	encryptionKey := os.Getenv("GO_ENCRYPTION_KEY")
-	ciphertext := encrypt([]byte("Hello World"), encryptionKey)
-	fmt.Printf("Encrypted: %x\n", ciphertext)
-	plaintext := decrypt(ciphertext, encryptionKey)
-	fmt.Printf("Decrypted: %s\n", plaintext)
+
+	keyPtr := flag.String("key", "", "Encryption/Decryption Key ")
+	stringPtr := flag.String("value", "", "string to encrypt or decrypt ")
+	encryptPtr := flag.Bool("encrypt", false, "encrypt action")
+	decryptPtr := flag.Bool("decrypt", false, "decrypt action")
+
+	flag.Parse()
+
+	//fmt.Println("key:", *keyPtr)
+	//fmt.Println("string:", *stringPtr)
+	//fmt.Println("encrypt?", *encryptPtr)
+	//fmt.Println("decrypt?", *decryptPtr)
+	//fmt.Println("tail:", flag.Args())
+
+	if *encryptPtr {
+		ciphertext := encrypt([]byte(*stringPtr), *keyPtr)
+		fmt.Printf("Encrypted: %x\n", ciphertext)
+	}
+	if *decryptPtr {
+		plaintext := decrypt([]byte(*stringPtr), *keyPtr)
+		fmt.Printf("Decrypted: %s\n", plaintext)
+	}
 	//encryptFile("sample.txt", []byte("Hello World"), "password1")
 	//fmt.Println(string(decryptFile("sample.txt", "password1")))
 }
